58de3be82d09094cab09fa0b86dd466a12580eb2
Juliusz Ceasar <marcin@o2.pl>
2024-02-11 11:17:33 +0100

    navbar, GNav, GButton

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,46 @@
+// ignore_for_file: prefer_const_constructors, prefer_final_fields
+
+import 'package:ecommerce_app_wzorzec/components/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'cart_page.dart';
+import 'shop_page.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update out selected index
+  // when the user taps on the bottom bar
+  navigatorBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage()
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigatorBottomBar(index),
+      ),
+    );
   }
 }
